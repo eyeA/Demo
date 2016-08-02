d9c09bc89103f60224f6e2f255469e0c9834ded7
eyea <1573765968@qq.com>
2016-08-02 19:34:10 +0800

    ng-定义定时器函数的正确姿势

--- AngularJS/self/Book-practice/第二章.js
+++ AngularJS/self/Book-practice/第二章.js
@@ -6,11 +6,10 @@ function my($scope,$timeout){
 		},1000);
 	};
 	updateClock();
-};
-
-function my($scope){
+	
+// 第二个
 	$scope.clock2 = {
-		now:new Date()
+		now:new Date(),
 	};
 	var updateClock2 = function(){
 		$scope.clock2.now = new Date()
@@ -19,4 +18,20 @@ function my($scope){
 		$scope.$apply(updateClock2);
 	},1000);
 	updateClock2();
-}
+
+};
+
+// function my($scope){
+// 	$scope.clock2 = {
+// 		now:new Date(),
+// 	};
+// 	var updateClock2 = function(){
+// 		$scope.clock2.now = new Date()
+// 	};
+// 	setInterval(function(){
+// 		$scope.$apply(updateClock2);
+// 	},1000);
+// 	updateClock2();
+// }
+
+// 分开分别写是 NG
