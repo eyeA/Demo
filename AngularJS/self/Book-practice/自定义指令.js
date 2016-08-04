ebcd94ed113330aa32c70bfd146d1fbe26462206
eyea <1573765968@qq.com>
2016-08-04 09:33:42 +0800

    ng--自定义指令my-directive--myDirective

---
+++ AngularJS/self/Book-practice/自定义指令.js
@@ -0,0 +1,23 @@
+var app = angular.module('myApp',[]);
+app.directive('myDirective',function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		restrict: 'E',
+		//link: function($scope, iElm, iAttrs, controller) {
+			
+		//}
+		template: '<a href="https://www.baidu.com/">直接到百度</a>'
+	};
+});
