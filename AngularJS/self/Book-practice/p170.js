eb008968ffa6c3dc490527fc99bb82c1c8338544
eyea <1573765968@qq.com>
2016-08-08 20:14:01 +0800

    .

--- AngularJS/self/Book-practice/p170.js
+++ AngularJS/self/Book-practice/p170.js
@@ -8,12 +8,13 @@ angular.module('myApp', [])
         $scope.pullRequests = data;
       })
 }])
-
+// 在angular中创建promise，可以使用内置的$q服务
+// 首先需要把$q服务注入到想要使用它的对象中
 .factory('GithubService', [
-  '$q', '$http',
+  '$q', '$http', // 现在就可以访问到$q库了
     function($q, $http) {
       var getPullRequests = function() {
-        var deferred = $q.defer();
+        var deferred = $q.defer(); // 创建一个deferred对象，可以调用defer方法
         // Get list of open angular js pull requests from github
         $http.get('https://api.github.com/repos/angular/angular.js/pulls')
         .success(function(data) {
