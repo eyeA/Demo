5f6e3aedbc12c5da1fdeb08c6cc2726433b6c967
eyea <1573765968@qq.com>
2016-08-08 10:52:24 +0800

    factory--加入点击统计次数的功能

--- AngularJS/self/Book-practice/p109.js
+++ AngularJS/self/Book-practice/p109.js
@@ -1,9 +1,12 @@
 var app = angular.module('myApp', []);
 
 app.factory('greeter', function(){
+	var count = 0;
 	return {
 		greet: function(msg){
 			alert(msg);
+			count++;
+			console.log(count)
 		},
 		end:function(msg){
 			alert(msg);
