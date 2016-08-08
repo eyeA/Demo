6df9e05e86adede17271c21ce2fd61da405fb8da
eyea <1573765968@qq.com>
2016-08-08 10:47:24 +0800

    factory定义多个函数的方法

--- AngularJS/self/Book-practice/p109.js
+++ AngularJS/self/Book-practice/p109.js
@@ -4,12 +4,16 @@ app.factory('greeter', function(){
 	return {
 		greet: function(msg){
 			alert(msg);
+		},
+		end:function(msg){
+			alert(msg);
 		}
 	}
 })
 
 .controller('myController',function($scope,greeter){
 	$scope.sayHello = function(){
-		greeter.greet('Hello')
+		greeter.greet('Hello');
+		greeter.end('over')
 	}
 })
