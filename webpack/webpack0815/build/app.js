3fb059aae51e06acad235c2e04f2e8035d37e953
eyea <1573765968@qq.com>
2016-08-15 18:07:23 +0800

    .

---
+++ webpack/webpack0815/build/app.js
@@ -0,0 +1,10 @@
+webpackJsonp([0,2],[
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	console.log('app.js');
+
+/***/ }
+]);
---
+++ webpack/webpack0815/build/index.js
@@ -0,0 +1,10 @@
+webpackJsonp([1,2],[
+/* 0 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	console.log('hello index.js');
+
+/***/ }
+]);
